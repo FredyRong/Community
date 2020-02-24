7211898344acce337d3042c975cf610e0c8a6cb4
fredy <419089660@qq.com>
2020-02-24 12:49:04 +0800

    修改migration

--- src/main/resources/db/migration/V11__Add_name_and_title_to_notification_table.sql
+++ src/main/resources/db/migration/V11__Add_name_and_title_to_notification_table.sql
@@ -1,3 +1,4 @@
 alter table notification add notifier_name varchar(100);
+alter table notification add notifier_name varchar(100);
 
 alter table notification add outer_title varchar(255);
--- src/main/resources/db/migration/V8__Change_creator_column_type.sql
+++ src/main/resources/db/migration/V8__Change_creator_column_type.sql
@@ -1,2 +1,2 @@
-alter table question alter column creator bigint;
-alter table comment alter column  commentator bigint;
+alter table question modify creator bigint;
+alter table comment modify commentator bigint;
